a40f3cecce2c384d8392dbc5f1b1afdba2a2c58a
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2025-02-14 19:27:38 +0200

    backup

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__,template_folder="templates")
 app.secret_key = "7u11t@nk"
 
 
-def render(url,title="null",user=None,inland={},coastal={}): 
+def render(url,title="null"): 
     return render_template(url,title=title)
 
 @app.route("/")
@@ -12,4 +12,4 @@ def home():
     return render("Home.html",title="Fulltanksa - Home")
 
 if __name__ == "__main__":
-    app.run( host='0.0.0.0', port=8765)
+    app.run( host='0.0.0.0', port=8765, debug=True)
