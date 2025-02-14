4f4a80f246ad3c170e419214717852797cdeec5c
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2025-02-14 18:32:46 +0200

    all

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for,send_from_directory , request, session,redirect,flash
+
+app = Flask(__name__,template_folder="templates")
+app.secret_key = "7u11t@nk"
+
+
+def render(url,title="null",user=None,inland={},coastal={}): 
+    return render_template(url,title=title)
+
+@app.route("/")
+def home():
+    return render("Home.html",title="Fulltanksa - Home")
+
+if __name__ == "__main__":
+    app.run(debug=True,port=4000)
